c9296f035d2c34f1c5600cdace1e7e4b5c3d8b11
leshey <leshey@SENIOR>
2017-06-10 06:47:17 +0300

    More the little correction

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,13 +1,48 @@
 package $package;
 
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import test.fx.view.LoaderController;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extend Application
 {
-    public static void main( String[] args )
+	private Stage primaryStage;
+	
+	public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	launch(args);
     }
+    
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+	    this.primaryStage = primaryStage;
+	    this.primaryStage.setTitle("Hello world application");
+	
+	    try {
+	        FXMLLoader loader = new FXMLLoader();
+	        
+	        loader.setLocation(getClass().getResource("/${package}/view/Main.fxml"));
+	        
+	        AnchorPane mainView = (AnchorPane) loader.load();
+	
+	        rootLayout.setCenter(mainView);
+	        
+	        LoaderController controller = loader.getController();
+	        controller.setApp(this);
+	    
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
 }
--- src/main/resources/archetype-resources/src/main/java/view/Controller.java
+++ src/main/resources/archetype-resources/src/main/java/view/Controller.java
@@ -1,9 +1,9 @@
 package ${package}.view;
 
-import java.awt.TextArea;
-
 import App;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class Controller 
 {
